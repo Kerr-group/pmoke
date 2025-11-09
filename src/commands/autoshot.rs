b8d523031ee1058e55e6dd073a7f109b9f7fc4c6
SoichiroYamane <soichiro.yamane.19970619@gmail.com>
2025-11-10 03:33:26 +0900

    refactor: autoshot.rs

--- src/commands/autoshot.rs
+++ src/commands/autoshot.rs
@@ -1,30 +1,16 @@
-use crate::communications::function_generator::FGHandler;
-use crate::communications::oscilloscope::OscilloscopeHandler;
+use crate::commands::single::single;
+use crate::commands::trigger::trigger;
 use crate::config::Config;
-use anyhow::{Context, Result};
-use colored::*;
+use anyhow::Result;
 use std::thread;
 use std::time::Duration;
 
 pub fn autoshot(cfg: &Config) -> Result<()> {
-    let mut handler = OscilloscopeHandler::initialize(cfg)
-        .context("failed to initialize oscilloscope handler")?;
-    handler
-        .set_single()
-        .context("failed to set oscilloscope to single mode")?;
-
-    println!(
-        "{}  Oscilloscope set to single mode successfully.",
-        "✔".green().bold()
-    );
+    single(cfg)?;
 
     thread::sleep(Duration::from_secs(1));
 
-    let mut handler =
-        FGHandler::initialize(cfg).context("failed to initialize function generator handler")?;
-    handler.trigger().context("failed to trigger")?;
-
-    println!("{}  Trigger command sent successfully.", "✔".green().bold());
+    trigger(cfg)?;
 
     Ok(())
 }
